723cc172485255fad76dd2e03e2442c7d131315f
Sara Wavde <sara.wavde@gmail.com>
2026-04-02 21:24:48 +0100

    Skeleton made for main.cpp, complete tmrw

---
+++ main.cpp
@@ -0,0 +1,25 @@
+// main.cxx
+#include "Particle.h"
+#include "FourMomentum.h"
+#include <iostream>
+#include <vector>
+
+int main(){
+
+// create particles
+
+// sum four momenta
+
+// dotp muons
+
+// copy assignment, electron to new electron
+
+// copy constructor: first muon to new muon
+
+// move constructor: antielectron
+
+// move assignment: antimuon
+
+// end of main, call destructors
+
+}
