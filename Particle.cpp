079dcbb9fdd7b0a2698cfd972f49f71b719df184
Sara Wavde <sara.wavde@gmail.com>
2026-04-02 21:26:53 +0100

    Skeleton made for Particle.cpp, write code tmrw

---
+++ Particle.cpp
@@ -0,0 +1,24 @@
+// Particle.cpp
+// Implementation of the Particle class
+#include "Particle.h"
+#include <iostream>
+#include <stdexcept>
+
+
+// valid particle names boolean
+
+// 
+// Parameterised constructor
+
+// Destructor
+
+
+// Copy constructor
+
+// Copy assignment
+
+// Move constructor
+// Move assignment
+
+// Getters
+// Print
